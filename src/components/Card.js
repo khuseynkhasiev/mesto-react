bd8bf2795fb4c8188119f830ccf3ea17eaa0117a
khuseynkhasiev <khuseyn-khasiev@mail.ru>
2023-01-02 10:02:56 +0300

    добавлен атрибут alt и картинки загружены через src

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card({card, handleDeleteCardClick, onCardClick}) {
         <li className="elements__el">
             <button className="elements__trash elements__trash_visible" type="button"
                     aria-label="кнопка удаления карточки" onClick={handleDeleteCardClick} ></button>
-            <img className="elements__img" onClick={onCardClick} style={{backgroundImage: `url(${card.link})`}}/>
+            <img className="elements__img" onClick={onCardClick} alt={card.name} src={card.link}/>
             <div className="elements__info">
                 <h2 className="elements__title">{card.name}</h2>
                 <div className="elements__counter">
